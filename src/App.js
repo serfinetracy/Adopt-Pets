3d7b7f60d0484d3931cb70e3f4aa74c7b48ddf9a
serfinetracy <tracyserfine@gmail.com>
2022-01-18 16:03:19 +0300

    additional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h3", {}, props.breed) ,   
-        ]);
-    };
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pet from "./Pet";
+
     const App = () => {
         return React.createElement("div", {},
 
